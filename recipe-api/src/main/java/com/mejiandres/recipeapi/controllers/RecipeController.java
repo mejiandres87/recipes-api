f2154670e06ee7302a70d0a7ba2e0dbc1b12cb3d
Andrés Felipe Mejía <mejia.andres87@gmail.com>
2023-10-01 20:41:44 -0500

    Adds temporary controller for recipes

---
+++ recipe-api/src/main/java/com/mejiandres/recipeapi/controllers/RecipeController.java
@@ -0,0 +1,33 @@
+package com.mejiandres.recipeapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mejiandres.recipeapi.models.dto.RecipeDto;
+import com.mejiandres.recipeapi.models.response.RecipeResponse;
+import com.mejiandres.recipeapi.services.RecipeService;
+import com.mejiandres.recipeapi.services.integration.RecipeFetchService;
+
+@RestController
+@RequestMapping("api")
+public class RecipeController {
+
+  @Autowired
+  RecipeService recipeService;
+
+  @GetMapping(path = "fetch-recipes", produces = "application/json")
+  public List<RecipeResponse> getRecipesBySearchTerm(@RequestParam String term) {
+    return recipeService.fetchAndStoreRecipes(term);
+  }
+
+  @GetMapping(path = "recipes", produces = "application/json")
+  public List<RecipeResponse> getAllRecipes() {
+    return recipeService.fetchStoredRecipes();
+  }
+
+}
