a6013a5226effe19728e1e491a4e6e5aa4dcd043
Andrés Felipe Mejía <mejia.andres87@gmail.com>
2023-10-01 20:14:47 -0500

    creates service to fetch recipes from 3rd party

---
+++ recipe-api/src/main/java/com/mejiandres/recipeapi/models/transfer/RecipesQueryResult.java
@@ -0,0 +1,26 @@
+package com.mejiandres.recipeapi.models.transfer;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RecipesQueryResult {
+
+  private List<Recipe> results;
+  private String baseUri;
+  private int offset;
+  private int number;
+  private int totalResults;
+  private Integer processingTimeMs;
+  private String expires;
+
+}
---
+++ recipe-api/src/main/java/com/mejiandres/recipeapi/services/integration/RecipeFetchService.java
@@ -0,0 +1,41 @@
+package com.mejiandres.recipeapi.services.integration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.mejiandres.recipeapi.models.dto.RecipeDto;
+import com.mejiandres.recipeapi.models.transfer.RecipesQueryResult;
+import com.mejiandres.recipeapi.utils.RecipesDataUtils;
+
+@Service
+public class RecipeFetchService {
+
+  @Autowired
+  private RestTemplate restTemplateBean;
+
+  @Value("${api.recipe.url}")
+  private String url;
+
+  @Value("${api.recipe.apiKey}")
+  private String apiKey;
+
+  public List<RecipeDto> fetchRecipes(String term) {
+    Map<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("query", term);
+    uriVariables.put("apiKey", "fbc1be1523784cf5a9ef551395585fbe");
+    RecipesQueryResult result = restTemplateBean.getForObject(url + "/recipes/search?query={query}&apiKey={apiKey}",
+        RecipesQueryResult.class,
+        uriVariables);
+    List<RecipeDto> recipes = result.getResults().stream().map(r -> RecipesDataUtils.dtoFromTransferRecipe(r)).toList();
+    return recipes;
+  }
+
+}
